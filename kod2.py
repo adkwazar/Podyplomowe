cafc8cbf82b54a26bfeb7cba59f799fc0f71cd19
adkwazar <40899969+adkwazar@users.noreply.github.com>
2025-11-04 12:00:10 +0100

    Update kod2.py

--- kod2.py
+++ kod2.py
@@ -50,3 +50,24 @@ class UNet(nn.Module):
             x = self.up_convs[i](x)
 
         return torch.sigmoid(self.final_conv(x))
+
+
+class DoubleConv(nn.Module):
+    """(conv => BN => ReLU) * 2"""
+    def __init__(self, in_channels, out_channels, mid_channels=None, dropout=0.0):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
