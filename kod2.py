cff37abf7021d08144ff06d522f0d83587243a92
adkwazar <40899969+adkwazar@users.noreply.github.com>
2025-11-04 12:03:46 +0100

    Update kod2.py

--- kod2.py
+++ kod2.py
@@ -1,73 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 class UNet(nn.Module):
-    def __init__(self, in_channels=1, out_channels=1, features=[64, 128, 256, 512], dropout=0.1):
+    def __init__(self):
         super(UNet, self).__init__()
 
-        # ENCODER
-        self.downs = nn.ModuleList()
-        self.pools = nn.ModuleList()
-        for feature in features:
-            self.downs.append(DoubleConv(in_channels, feature, dropout=dropout))
-            self.pools.append(nn.MaxPool2d(kernel_size=2, stride=2))
-            in_channels = feature
-
-        # BOTTLENECK
-        self.bottleneck = DoubleConv(features[-1], features[-1]*2, dropout=dropout)
-
-        # DECODER
-        self.ups = nn.ModuleList()
-        self.up_convs = nn.ModuleList()
-        for feature in reversed(features):
-            self.ups.append(
-                nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2)
-            )
-            self.up_convs.append(DoubleConv(feature*2, feature, dropout=dropout))
-
-        # OUTPUT
-        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
-
-    def forward(self, x):
-        skip_connections = []
-
-        # ENCODER PATH
-        for i in range(len(self.downs)):
-            x = self.downs[i](x)
-            skip_connections.append(x)
-            x = self.pools[i](x)
-
-        x = self.bottleneck(x)
-        skip_connections = skip_connections[::-1]
-
-        # DECODER PATH
-        for i in range(len(self.ups)):
-            x = self.ups[i](x)
-            skip_connection = skip_connections[i]
-
-            # dopasuj rozmiar (na wypadek zaokrągleń)
-            if x.shape != skip_connection.shape:
-                x = F.interpolate(x, size=skip_connection.shape[2:])
-
-            x = torch.cat((skip_connection, x), dim=1)
-            x = self.up_convs[i](x)
-
-        return torch.sigmoid(self.final_conv(x))
-
-
-class DoubleConv(nn.Module):
-    """(conv => BN => ReLU) * 2"""
-    def __init__(self, in_channels, out_channels, mid_channels=None, dropout=0.0):
-        super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
+        # Enkoder
+        self.enc_conv1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Dropout(dropout)
+            nn.Conv2d(16, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True)
         )
 
-    def forward(self, x):
-        return self.double_conv(x)
+        self.enc_conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True)
+        )
 
+        self.enc_conv3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # Dekoder
+        self.up1 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
+        self.dec_conv1 = nn.Sequential(
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True)
+        )
+
+        self.up2 = nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2)
+        self.dec_conv2 = nn.Sequential(
+            nn.Conv2d(32, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True)
+        )
+
+        self.final_conv = nn.Conv2d(16, 1, kernel_size=1)
+
+    def forward(self, x):
+        # Encoder
+        x1 = self.enc_conv1(x)
+        x1p = self.pool(x1)
+
+        x2 = self.enc_conv2(x1p)
+        x2p = self.pool(x2)
+
+        x3 = self.enc_conv3(x2p)
+
+        # Decoder
+        x4 = self.up1(x3)
+        if x4.shape != x2.shape:
+            x4 = F.interpolate(x4, size=x2.shape[2:])
+        x4 = torch.cat([x4, x2], dim=1)
+        x4 = self.dec_conv1(x4)
+
+        x5 = self.up2(x4)
+        if x5.shape != x1.shape:
+            x5 = F.interpolate(x5, size=x1.shape[2:])
+        x5 = torch.cat([x5, x1], dim=1)
+        x5 = self.dec_conv2(x5)
+
+        out = torch.sigmoid(self.final_conv(x5))
+        return out
